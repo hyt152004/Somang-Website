07a38a1afc6da3547c9d6588250ee0b9a2dd4590
david <hyt152004@gmail.com>
2024-08-28 12:23:55 -0700

    made small adjustments with the layout

--- src/App.jsx
+++ src/App.jsx
@@ -38,12 +38,14 @@ function App() {
             <br/>
             (Korean Foursquare Gospel Church)
           </h1>
-          <h2 className='header-subtitle'>
-            원로목사: 남상국 담임목사: 홍석인
-          </h2>
-          <p className='header-contact'>
-            5825 Nelson Ave Burnaby BC V5H 3H6 / 604-437-5469
-          </p>
+          <br/>
+          <br/>
+          <h1 className='header-title'>
+          예수님 이야기로 가득한 교회 (italics this) (add dcolour to this (strong colour))
+          </h1>
+          {/* <h2 className='header-subtitle'>
+            <span className='pastorName'>예수님 이야기로 가득한 교회</span>
+          </h2> */}
         </div>
       </div>
 
@@ -52,16 +54,16 @@ function App() {
 
       <fieldset className="grid-container">
         <legend>예배시간</legend>
-          <p>주일 예배: 오후 2시</p>
+          <p>주일 예배 : 오후 2시</p>
           {/* make this bigger */}
-          <p>소망 YG (Young Generation): 오후 2시</p>
+          <p>소망 YG (Young Generation) : 오후 2시</p>
       </fieldset>
 
       <fieldset className="grid-container">
         <legend>기도회</legend>
-          <p>아침기도회: 아침 8시 (화~목)</p>
+          <p>아침기도회 : 아침 8시 (화~목)</p>
           {/* make this bigger */}
-          <p>금요기도회: 저녁 8시</p>
+          <p>금요기도회 : 저녁 8시</p>
       </fieldset>
 
       <fieldset className="grid-container">
@@ -88,23 +90,24 @@ function App() {
 
 
       <fieldset className="grid-container">
-        <legend>클릭! 찬약 / 설교 다시보기!</legend>
-        <a href="https://www.youtube.com/@Jwpark57">https://www.youtube.com/@Jwpark57</a>
+        <legend>클릭! 찬양 / 설교 보기</legend>
+        <div className="youtubeLinkContainer">
+          <a className='youtubeLink' href="https://www.youtube.com/@Jwpark57">https://www.youtube.com/@Jwpark57</a>
+        </div>
       </fieldset>
 
       <fieldset className="grid-container">
-        <legend>클릭! 찬약 / 설교 다시보기!</legend>
+        <legend>교회 연락처</legend>
           <p>전화: 604-437-5469</p>
           <p>주소: 5825 Nelson Ave, Burnaby, BC, V5H 3H6</p>
         <div className='map-container'>
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2557.864765044608!2d-122.99729162351406!3d49.2316961713852!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54867657c6e0d855%3A0x7956374b1411816f!2s5825%20Nelson%20Ave%2C%20Burnaby%2C%20BC%20V5H%203H6!5e1!3m2!1sen!2sca!4v1724457634369!5m2!1sen!2sca"
+            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d5210.716568619125!2d-122.99471700000001!3d49.231696!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54867657c12591c9%3A0x8c0b98ccd9354129!2s5825%20Nelson%20Ave%2C%20Burnaby%2C%20BC%20V5H%203H6!5e0!3m2!1sen!2sca!4v1724795429370!5m2!1sen!2sca"
             width="600"
             height="450"
             style={{ border: 0 }}
             allowFullScreen=""
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade">
+            loading="lazy">
           </iframe>
         </div>
       </fieldset>
@@ -115,9 +118,14 @@ function App() {
 
 export default App;
 
+// change header colour
+// make login to add photos?
+// bible and cross background adjustment for small screen
+// think about how gallery can be implemented better
+// add box color. no white wanted
+// make a better system for slideshow
 
-// - add headers for each section !!!
-// - make login to add photos?
-// - bible and cross background 
-// - think about how gallery can be implemented better
-// - add box color. no white wanted
+
+
+// spacing between pastor name
+// make font size bigger
